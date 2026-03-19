e7e66704c7175cba84c0ed327f977eaa3916149f
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2026-03-19 17:45:48 -0600

    Add sub-dropdowns

--- src/components/base/Dropdown.tsx
+++ src/components/base/Dropdown.tsx
@@ -1,4 +1,7 @@
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuItemProps, DropdownMenuPortal, DropdownMenuTrigger }
+import {
+    DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuItemProps, DropdownMenuPortal,
+    DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger,
+}
     from "@radix-ui/react-dropdown-menu";
 import { Check } from "lucide-react";
 
@@ -31,21 +34,55 @@ export function DropdownContent({ children }: React.PropsWithChildren) {
     );
 }
 
-export function DropdownItem({ children, danger, onClick, ...props }: { danger?: boolean, onClick?: () => void, selected?: boolean }
+export function DropdownItem({ children, danger, onClick, ...props }: {
+    danger?: boolean,
+    onClick?: (e: MouseEvent) => void,
+    selected?: boolean
+}
 & DropdownMenuItemProps) {
     return (
         <DropdownMenuItem
             {...props}
             className={`${danger && "text-danger"} px-2 py-1 rounded-lg 
-                cursor-pointer outline-none transition-all hover:bg-bg-lightest ${props.className}`
+                cursor-pointer outline-none transition-all hover:bg-bg-lightest ${props.className}
+                ${props.selected && "flex items-center gap-1"}`
             }
             style={props.style}
             onClick={e => {
                 e.stopPropagation();
-                onClick?.();
+                onClick?.(e);
             }}
         >
-            {props.selected && <Check />} {children}
+            {props.selected && <Check size={20} className="text-gray-500" />} {children}
         </DropdownMenuItem>
+    );
+}
+
+export function DropdownSub({ children }: React.PropsWithChildren) {
+    return (
+        <DropdownMenuSub>
+            {children}
+        </DropdownMenuSub>
+    );
+}
+
+export function DropdownSubTrigger({ asChild, children }: { asChild?: boolean } & React.PropsWithChildren) {
+    return (
+        <DropdownMenuSubTrigger asChild={asChild}>
+            {children}
+        </DropdownMenuSubTrigger>
+    );
+}
+
+export function DropdownSubContent({ children }: React.PropsWithChildren) {
+    return (
+        <DropdownMenuPortal>
+            <DropdownMenuSubContent
+                className={`p-2 bg-bg-lighter rounded-lg text-sm flex flex-col min-w-37.5
+                    fade-in z-200 drop-shadow-lg max-h-100 overflow-scroll`}
+            >
+                {children}
+            </DropdownMenuSubContent>
+        </DropdownMenuPortal>
     );
 }
